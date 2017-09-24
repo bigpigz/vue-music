f7f6e3450ace0b7126cdbd8111de4892d3ba1753
bigz <493971184@qq.com>
2017-09-24 20:01:22 +0800

    0项目初始化

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    hello world
+    <!--<router-view></router-view>-->
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'app'
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "common/stylus/variable.styl";
+  #app
+    color: $color-theme
+</style>
