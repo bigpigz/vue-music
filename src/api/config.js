bed136efd2c6dcad6961a5e6b332e2db2112c2a3
bigz <493971184@qq.com>
2017-09-25 17:04:20 +0800

    4-3 jsonp应用和轮播图数据抓取

---
+++ src/api/config.js
@@ -0,0 +1,20 @@
+ /**
+ * Created by BigZ on 17/9/25.
+ */
+ export const commonParams = {
+   g_tk:1706583502,
+   // format:'json',
+   format:'jsonp',
+   inCharset:'utf-8',
+   outCharset:'utf-8',
+   notice:0
+   // platform:h5,
+   // uin:493971184,
+   // needNewCode:1,
+   // _:1504399133787
+ }
+ export const options = {
+   param: 'jsonpCallback'
+ }
+
+ export const ERR_OK = 0
--- src/common/js/jsonp.js
+++ src/common/js/jsonp.js
@@ -1,13 +1,13 @@
 /**
  * Created by BigZ on 17/9/25.
  */
-import orginJSONP from 'jsonp'
+import originJsonp from 'jsonp'
 
 export default function jsonp(url, data, option) {
   //如果没有问号，第一个拼接一个问号
   url += (url.indexOf('?')<0 ? '?' : '&') +param(data)
   return new Promise((resolve, reject) => {
-    orginJSONP(url, option, (err, data) => {
+    originJsonp(url, option, (err, data) => {
       if (!err) {
         resolve(data)
       } else {
@@ -17,11 +17,13 @@ export default function jsonp(url, data, option) {
   })
 }
 
-function param(data) {
+export function param(data) {
   let url = ''
   for (var k in data) {
     let value = data[k] !== undefined ? data[k] : ''
-    url += `&${k}=${encodeURIComponent(value)}`
+    //url += `&${k}=${encodeURIComponent(value)}`
+    url += '&' + k + '=' + encodeURIComponent(value)
+    //encodeURIComponent() 函数可把字符串作为 URI 组件进行编码。
   }
   //如果有data 去掉第一个& ，没有就返回空''
   return url ? url.substring(1) : ''
