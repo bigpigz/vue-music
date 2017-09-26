1c5fa64a1061daac3a49f17f9a8a27f71813f2df
bigz <493971184@qq.com>
2017-09-26 14:07:19 +0800

    4-9 歌单列表组件开发和数据的应用

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -2,18 +2,26 @@
   <div class="recommend">
     <div class="recommend-content">
       <div class="slider-wrapper" v-if="recommends.length">
-          <slider>
-            <div v-for="item in recommends">
-              <a :href="item.linkUrl">
-                <img :src="item.picUrl">
-              </a>
-            </div>
-          </slider>
+        <slider>
+          <div v-for="item in recommends">
+            <a :href="item.linkUrl">
+              <img :src="item.picUrl">
+            </a>
+          </div>
+        </slider>
       </div>
       <div class="recommend-list">
         <h1 class="list-title">热门歌单推荐</h1>
         <ul>
-
+            <li v-for="item in discList" class="item">
+              <div class="icon">
+                <img :src="item.imgurl" width="60" height="60">
+              </div>
+              <div class="text">
+                <h2 class="name" v-html="item.creator.name"></h2>
+                <p class="desc" v-html="item.dissname"></p>
+              </div>
+            </li>
         </ul>
       </div>
     </div>
@@ -21,14 +29,15 @@
 </template>
 
 <script type="text/ecmascript-6">
-  import {getRecommend,getDiscList} from 'api/recommend'
+  import {getRecommend, getDiscList} from 'api/recommend'
   import {ERR_OK} from 'api/config'
   import Slider from 'base/slider/slider'
   export default{
     data(){
-        return {
-          recommends:[]
-        }
+      return {
+        recommends: [],
+        discList: []
+      }
     },
     created(){
       this._getRecommend()
@@ -43,14 +52,14 @@
         })
       },
       _getDiscList(){
-        getDiscList().then((res)=>{
+        getDiscList().then((res) => {
           if (res.code === ERR_OK) {
-            console.log(res.data.list);
+            this.discList = res.data.list;
           }
         })
       }
     },
-    components:{
+    components: {
       Slider
     }
   }
