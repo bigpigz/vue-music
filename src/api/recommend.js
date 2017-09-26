89160bc57074c0fae4cbeeaaa395fb92513a7de3
bigz <493971184@qq.com>
2017-09-26 13:56:40 +0800

    4-8 介绍和后端接口代理

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -3,7 +3,7 @@
  */
 import jsonp from 'common/js/jsonp'
 import {commonParams, options} from './config'
-
+import axios from 'axios'
 
 export function getRecommend() {
   const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
@@ -16,33 +16,24 @@ export function getRecommend() {
 }
 
 export function getDiscList() {
-  const url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
+  const url = '/api/getDiscList'
   const data = Object.assign({}, commonParams, {
     platform: 'yqq',
-    hostUin:0,
-    sin:0,
-    ein:29,
-    sortId:5,
-    needNewCode:0,
-    categoryId:10000000,
-    rnd:Math.random()
+    hostUin: 0,
+    sin: 0,
+    ein: 29,
+    sortId: 5,
+    needNewCode: 0,
+    categoryId: 10000000,
+    rnd: Math.random(),
+    format: 'json'
 
   })
-  return jsonp(url, data, options)
+  return axios.get(url, {
+    params: data
+  }).then((res) => {
+    return Promise.resolve(res.data)
+  })
 }
 
-/*rnd:0.25785322317711423,
-  g_tk:2097507928,
-  jsonpCallback:'getPlaylist',
-  loginUin:493971184,
-  hostUin:0,
-  format:'jsonp',
-  inCharset:'utf8',
-  outCharset:'utf-8',
-  notice:0,
-  platform:'yqq',
-  needNewCode:0,
-  categoryId:10000000,
-  sortId:5,
-  sin:0,
-  ein:29*/
+
