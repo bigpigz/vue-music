2df7350a9d94ffcb9c0af823d5acc45bc5b590aa
bigz <493971184@qq.com>
2017-09-26 15:29:35 +0800

    4-12 vue-lazyload懒加载插件介绍和应用

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -6,7 +6,7 @@
           <slider>
             <div v-for="item in recommends">
               <a :href="item.linkUrl">
-                <img :src="item.picUrl" @load="loadImage">
+                <img :src="item.picUrl" @load="loadImage" class="needsclick">
               </a>
             </div>
           </slider>
@@ -16,7 +16,7 @@
           <ul>
             <li v-for="item in discList" class="item">
               <div class="icon">
-                <img :src="item.imgurl" width="60" height="60">
+                <img v-lazy="item.imgurl" width="60" height="60">
               </div>
               <div class="text">
                 <h2 class="name" v-html="item.creator.name"></h2>
