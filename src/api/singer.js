c61db5d5725c96d6359a904e0be0cb76bc20ffcf
bigz <493971184@qq.com>
2017-09-27 16:16:40 +0800

    6-5 歌手详情数据抓取

--- src/api/singer.js
+++ src/api/singer.js
@@ -19,19 +19,20 @@ export function getSingerList() {
   })
   return jsonp(url, data, options)
 }
-/*
-channel:singer
-page:list
-key:all_all_all
-pagesize:100
-pagenum:1
-g_tk:2097507928
-jsonpCallback:GetSingerListCallback
-loginUin:493971184
-hostUin:0
-format:jsonp
-inCharset:utf8
-outCharset:utf-8
-notice:0
-platform:yqq
-needNewCode:0*/
+
+export function getSingerDetail(singerId) {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_singer_track_cp.fcg'
+  const data = Object.assign({}, commonParams, {
+    hostUin:0,
+    needNewCode:0,
+    platform:'yqq',
+    order:'listen',
+    begin:0,
+    num:100,
+    songstatus:1,
+    singermid:singerId,
+    g_tk:2097507928
+  })
+  return jsonp(url, data, options)
+}
+
--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -8,6 +8,8 @@
 
 <script type="text/ecmascript-6">
   import {mapGetters} from 'vuex'
+  import {getSingerDetail} from 'api/singer'
+  import {ERR_OK} from 'api/config'
   export default{
     computed: {
       ...mapGetters([
@@ -15,7 +17,21 @@
       ])
     },
     created(){
+      this._getDetail()
       console.log(this.singer);
+    },
+    methods: {
+      _getDetail(){
+        if (!this.singer.id) {
+          //判断如果没有获取到id 直接回到指定路由
+          this.$router.push('/singer')
+        }
+        getSingerDetail(this.singer.id).then((res) => {
+          if (res.code === ERR_OK) {
+            console.log(res.data.list);
+          }
+        })
+      }
     }
   }
 
