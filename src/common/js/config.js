86bf7d7c3f69dd68a820808718fef9c53572401b
bigz <493971184@qq.com>
2017-09-28 15:53:47 +0800

    7-3 播放器Vuex的相关应用

---
+++ src/common/js/config.js
@@ -0,0 +1,8 @@
+/**
+ * Created by BigZ on 17/9/28.
+ */
+export const playMode = {
+  sequence: 0,
+  loop: 1,
+  random: 2
+}
--- src/store/state.js
+++ src/store/state.js
@@ -1,8 +1,15 @@
 /**
  * Created by BigZ on 17/9/27.
  */
+import {playMode} from 'common/js/config'
 const state = {
-  singer: {}
+  singer: {},
+  playing: false,
+  fullScreen: false,
+  playlist: [],
+  sequenceList: [],
+  mode: playMode.sequence,
+  currentIndex: -1
 }
 
 export default state
