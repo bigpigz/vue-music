70ae9d9337211dabca864a2248d8b6db57ed35ad
bigz <493971184@qq.com>
2017-09-28 14:39:18 +0800

    6-12 music-list 组件开发(5) 图片下拉缩放功能

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -64,9 +64,21 @@
       //跟随滚动效果
       scrollY(newY){
         let zIndex = 0
+        let scale = 1
+        let blur = 0
         let translateY = Math.max(this.minTranslateY, newY)
         this.$refs.layer.style['transform'] = `translate3d(0,${translateY}px,0)`
         this.$refs.layer.style['webkitTransform'] = `translate3d(0,${translateY}px,0)`
+        const percent = Math.abs(newY / this.imageHeight)
+        if (newY > 0) {
+          scale = 1 + percent
+          zIndex = 10
+        } else {
+          blur = Math.min(20 * percent, 20)
+        }
+        //backdrop-filter 是ios 的高斯模糊的样式  安卓是看不见的
+        this.$refs.filter.style['backdrop-filter'] = `blur${blur}px`
+        this.$refs.filter.style['webkitBackdrop-filter'] = `blur${blur}px`
         if (newY < this.minTranslateY) {
           zIndex = 10
           this.$refs.bgImage.style.paddingTop = 0
@@ -77,6 +89,8 @@
           this.$refs.bgImage.style.zIndex = 0
         }
         this.$refs.bgImage.style.zIndex = zIndex
+        this.$refs.bgImage.style['transform'] = `scale(${scale})`
+        this.$refs.bgImage.style['webkitTransform'] = `scale(${scale})`
       }
     },
     created(){
