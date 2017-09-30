0ccc2cf454564abb1dc6d15c312c6d4b93fc702f
bigz <493971184@qq.com>
2017-09-30 11:07:42 +0800

    7-20 播放器歌词数据解析

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -84,6 +84,7 @@
   import ProgressCircle from 'base/progress-circle/progress-circle'
   import {playMode} from 'common/js/config'
   import {shuffle} from 'common/js/util'
+  import Lyric from 'lyric-parser'
   const transform = prefixStyle('transform')
 
   export default {
@@ -91,7 +92,8 @@
       return {
         songReady: false,
         currentTime: 0,
-        radius: 32
+        radius: 32,
+        currentLyric: null
       }
     },
     computed: {
@@ -248,6 +250,12 @@
         })
         this.setCurrentIndex(index)
       },
+      getLyric(){
+        this.currentSong.getLyric().then((lyric) => {
+            this.currentLyric = new Lyric(lyric)
+          console.log(this.currentLyric);
+        })
+      },
       _pad(num, n = 2){
         let len = num.toString().length
         while (len < n) {
@@ -286,7 +294,7 @@
         }
         this.$nextTick(() => {
           this.$refs.audio.play()
-          this.currentSong.getLyric()
+          this.getLyric()
         })
       },
       playing(newPlaying){
