1299abe1da5784e62b23b3c50638fc4cdf5b470e
bigz <493971184@qq.com>
2017-10-05 17:16:33 +0800

    10-18 底部高度自适应 mixin应用

--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -3,7 +3,7 @@
     <div class="search-box-wrapper">
       <search-box ref="searchBox" @query="onQueryChange"></search-box>
     </div>
-    <div class="shortcut-wrapper" v-show="!query">
+    <div class="shortcut-wrapper" v-show="!query" ref="shortcutWrapper">
       <scroll class="shortcut" :data="shortcut" ref="shortcut">
         <div>
           <div class="hot-key">
@@ -30,8 +30,8 @@
         </div>
       </scroll>
     </div>
-    <div class="search-result" v-show="query">
-      <suggest @select="saveSearch" @listScroll="blurInput" :query="query"></suggest>
+    <div class="search-result" v-show="query" ref="searchResult">
+      <suggest ref="suggest" @select="saveSearch" @listScroll="blurInput" :query="query"></suggest>
     </div>
     <confirm ref="confirm"
              text="是否清空所有搜索历史"
@@ -50,9 +50,11 @@
   import SearchList from 'base/search-list/search-list'
   import Confirm from 'base/confirm/confirm'
   import Scroll from 'base/scroll/scroll'
+  import {playlistMixin} from 'common/js/mixin'
   import {mapActions, mapGetters} from 'vuex'
 
   export default {
+    mixins: [playlistMixin],
     created(){
       this._getHotKey()
     },
@@ -72,6 +74,13 @@
       ])
     },
     methods: {
+      handlePlaylist(playlist){
+        const bottom = playlist.length > 0 ? '60px' : ''
+        this.$refs.shortcutWrapper.style.bottom = bottom
+        this.$refs.searchResult.style.bottom = bottom
+        this.$refs.shortcut.refresh()
+        this.$refs.suggest.refresh()
+      },
       addQuery(query){
         this.$refs.searchBox.setQuery(query)
       },
