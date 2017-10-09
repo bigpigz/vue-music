a5b1068e420dba90033ece5dc6076b0abaf696a6
bigz <493971184@qq.com>
2017-10-09 16:35:00 +0800

    11-7 playlist 清空播放列表功能

--- src/store/actions.js
+++ src/store/actions.js
@@ -101,9 +101,18 @@ export const deleteSong = function ({commit, state}, song) {
   commit(types.SET_SEQUENCE_LIST, sequenceList)
   commit(types.SET_CURRENT_INDEX, currentIndex)
 
-  if (!playlist.length) {
-    commit(types.SET_PLAYING_STATE, false)
-  }else{
-    commit(types.SET_PLAYING_STATE, true)
-  }
+  const playingState = playlist.length > 0
+  commit(types.SET_PLAYING_STATE, playingState)
+  /*if (!playlist.length) {
+   commit(types.SET_PLAYING_STATE, false)
+   }else{
+   commit(types.SET_PLAYING_STATE, true)
+   }*/
+}
+
+export const deleteSongList = function ({commit}) {
+  commit(types.SET_PLAYLIST, [])
+  commit(types.SET_SEQUENCE_LIST, [])
+  commit(types.SET_CURRENT_INDEX, -1)
+  commit(types.SET_PLAYING_STATE, false)
 }
