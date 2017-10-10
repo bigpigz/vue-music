8efb87d5c460e55e947b6880988308f70de2cc61
bigz <493971184@qq.com>
2017-10-10 21:51:01 +0800

    11-12 switche及tab切换

--- src/components/add-song/add-song.vue
+++ src/components/add-song/add-song.vue
@@ -11,6 +11,7 @@
         <search-box @query="onQueryChange" placeholder="搜索歌曲"></search-box>
       </div>
       <div class="shortcut" v-show="!query">
+        <switches :switches="switches" :currentIndex="currentIndex" @switch="switchItem"></switches>
       </div>
       <div class="search-result" v-show="query">
         <suggest @listScroll="blurInput" :showSinger="showSinger" :query="query" @select="selectSuggest"></suggest>
@@ -23,12 +24,22 @@
   import SearchBox from 'base/search-box/search-box'
   import Suggest from 'components/suggest/suggest'
   import {searchMixin} from 'common/js/mixin'
+  import Switches from 'base/switches/switches'
   export default {
     mixins: [searchMixin],
     data(){
       return {
         showFlag: false,
-        showSinger: false
+        showSinger: false,
+        currentIndex: 0,
+        switches: [
+          {
+            name: '最近播放'
+          },
+          {
+            name: '搜索历史'
+          }
+        ]
       }
     },
     methods: {
@@ -40,11 +51,15 @@
       },
       selectSuggest(){
         this.saveSearch()
+      },
+      switchItem(index){
+          this.currentIndex = index
       }
     },
     components: {
       SearchBox,
-      Suggest
+      Suggest,
+      Switches
     }
   }
 </script>
